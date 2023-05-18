1225b39befc6e6b4179e7e072cbcd75c997755ea
Basit-786 <arsajid090@gmail.com>
2023-05-18 20:38:52 +0500

    initial comment

---
+++ script.js
@@ -0,0 +1,33 @@
+let btn = document.querySelector(".press-btn");
+let icons = Array.from(document.querySelectorAll(".icon"));
+let dailog = document.querySelector(".dailog");
+let cross = document.querySelector(".cross");
+
+let iconsWrapper = document.querySelector(".icons");
+
+btn.addEventListener("click", () => {
+  btn.setAttribute("src", "/pic/pressed.png");
+  btn.style.filter = "grayscale(100%)";
+  iconsWrapper.classList.add("rotate");
+
+  //   Selecting a Random Icon
+
+  let random = Math.round(Math.random() * (icons.length - 1));
+  let win = icons[random];
+  console.log(win);
+  setTimeout(() => {
+    btn.setAttribute("src", "/pic/press.png");
+    iconsWrapper.classList.remove("rotate");
+    win.classList.add("win");
+  }, 2000);
+
+
+  setTimeout(() => {
+    dailog.style.display = "flex";
+    win.classList.remove("win");
+  }, 3000);
+});
+
+cross.addEventListener("click", () => {
+  dailog.style.display = "none";
+});
